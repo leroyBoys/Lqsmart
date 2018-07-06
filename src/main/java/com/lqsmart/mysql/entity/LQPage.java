e3d8de9462bd8179640e6e06bbe5fb7eab65d2ab
leroyBoys <wlvxiaohui@163.com>
2018-07-06 19:12:44 +0800

    添加数据类型接口支持

--- src/main/java/com/lqsmart/mysql/entity/LQPage.java
+++ src/main/java/com/lqsmart/mysql/entity/LQPage.java
@@ -88,6 +88,14 @@ public class LQPage<T> {
         return allCount;
     }
 
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
     public void setAllCount(int allCount) {
         this.allCount = allCount;
     }
